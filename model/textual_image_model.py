9b281c599b602921e2b1a2814f9c67dbb585982b
SangPham <phamphuocsang2834@gmail.com>
2024-06-10 20:39:04 +0700

    eqwe

--- model/textual_image_model.py
+++ model/textual_image_model.py
@@ -365,9 +365,9 @@ class Textual_Image_Model(nn.Module):
         # 4. Contrastive Loss
         if self.training:
             loss=self.constrasive_loss(texts_feat,decoder_feats,labels,n=n)
-            return dict({"logits": logits,"loss":loss}  )
+            return dict({"scores": logits,"loss":loss}  )
         else:
-            return dict({"logits": logits})
+            return dict({"scores": logits})
 
 
 def build_textual_image_model(config: dict):
