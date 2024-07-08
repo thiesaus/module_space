84fd5b86caf0161f3a459249e28c78e91d43e6b8
SangPham <phamphuocsang2834@gmail.com>
2024-07-08 08:18:41 +0700

    tra

--- data/dataloader.py
+++ data/dataloader.py
@@ -839,3 +839,125 @@ class Track_Dataset(Dataset):
 
     def __len__(self):
         return len(self.data)
+
+class Track_MeMOTR(Dataset):
+    def __init__(self, mode, opt):
+        self.opt = opt
+        self.mode = mode
+        self.transform = {idx: get_transform(self.mode, self.opt, idx) for idx in (0, 1, 2)}
+        self.data = self._parse_data()
+        # bdd100k || mot17 
+
+    def _parse_data(self):
+        sample_length = self.opt["sample_frame_len"]
+        sample_stride = self.opt["sample_frame_stride"]
+        DATA = list()
+        for video in VIDEOS[self.mode]:
+            # load tracks
+            tracks_1 = np.loadtxt(join(self.opt["memotr_root"],self.opt['memotr_checkpoint'].upper(),self.opt, video, 'car', 'predict.txt'), delimiter=',')
+            if len(tracks_1.shape) == 2:
+                tracks = tracks_1
+                max_obj_id = max(tracks_1[:, 1])
+            else:
+                tracks = np.empty((0, 10))
+                max_obj_id = 0
+            tracks_2 = np.loadtxt(join(self.opt["memotr_root"],self.opt['memotr_checkpoint'].upper(), video, 'pedestrian', 'predict.txt'), delimiter=',')
+            if len(tracks_2.shape) == 2:
+                tracks_2[:, 1] += max_obj_id
+                tracks = np.concatenate((tracks, tracks_2), axis=0)
+            tracks = tracks[np.lexsort([tracks[:, 0], tracks[:, 1]])]  # ID->frame
+            # parse tracks
+            ids = set(tracks[:, 1])
+            for obj_id in ids:
+                tracks_id = tracks[tracks[:, 1] == obj_id]
+                frame_min, frame_max = int(min(tracks_id[:, 0])), int(max(tracks_id[:, 0]))
+                # 识别轨迹断点位置，从而方便对每个sub-tracklet单独处理
+                frame_pairs, start_frame, stop_frame = list(), frame_min, -1
+                previous_frame = start_frame - 1
+                for frame_idx in list(tracks_id[:, 0]) + [1e5]:
+                    if frame_idx != previous_frame + 1:
+                        stop_frame = previous_frame
+                        frame_pairs.append([int(start_frame), int(stop_frame)])
+                        start_frame = frame_idx
+                    previous_frame = frame_idx
+                # 将tracklets按sample_stride划分为片段
+                total_length = 0
+                for f_min, f_max in frame_pairs:
+                    total_length += (f_max - f_min + 1)
+                    for f_idx in range(f_min, f_max + 1, sample_stride):
+                        f_stop = min(f_max, f_idx + sample_length - 1)
+                        f_start = max(f_min, f_stop - sample_length + 1)
+                        tracklets = tracks_id[np.isin(
+                            tracks_id[:, 0],
+                            range(f_start, f_stop + 1)
+                        )][:, :6]
+                        tracklets[:, 4:6] += tracklets[:, 2:4]
+                        tracklets = tracklets.astype(int)
+                        assert (f_stop - f_start + 1) == len(tracklets)
+                        for expression in EXPRESSIONS[video]:
+                            DATA.append(dict(
+                                video=video,
+                                obj_id=int(obj_id),
+                                start_frame=f_start,
+                                stop_frame=f_stop,
+                                tracklets=tracklets,
+                                expression=expression,
+                            ))
+                        if f_stop == f_max:
+                            break
+                assert total_length == len(tracks_id)
+        return DATA
+
+    def __getitem__(self, index):
+        video, obj_id, start_frame, stop_frame, tracklets, expression = self.data[index].values()
+        assert (stop_frame - start_frame + 1) == len(tracklets)
+
+        # expression conversion
+        expression_converted = expression_conversion(expression)
+
+        # frame sampling
+        sampled_indices = np.linspace(
+            0, len(tracklets),
+            self.opt["sample_frame_num"],
+            endpoint=False, dtype=int
+        )
+        sampled_tracklets = tracklets[sampled_indices]
+
+        # load images
+        images = [
+            Image.open(
+                join(
+                    self.opt["data_root"],
+                    'KITTI/training/image_02/{}/{:0>6d}.png'
+                        .format(video, bbox[0])
+                )
+            ) for bbox in sampled_tracklets
+        ]
+
+        # crop images
+        cropped_images = torch.stack(
+            [self.transform[0](
+                images[i].crop(bbox[2:6])
+            ) for i, bbox in enumerate(sampled_tracklets)],
+            dim=0
+        )
+
+        # global images
+        global_images = torch.stack([
+            self.transform[2](image)
+            for image in images
+        ], dim=0)
+
+        return dict(
+            video=video,
+            obj_id=obj_id,
+            start_frame=start_frame,
+            stop_frame=stop_frame,
+            cropped_images=cropped_images,
+            global_images=global_images,
+            expression_raw=expression,
+            expression_new=expression_converted,
+        )
+
+    def __len__(self):
+        return len(self.data)
